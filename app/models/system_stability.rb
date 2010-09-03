c02f51010a8c04822a72310c845c71baca284e2d
Somchok Sakjiraphong <s@sunny.in.th>
2010-09-04 06:07:31 +0700

    Updated N1Criteria and SystemStability to insert null for blank values.

--- app/models/system_stability.rb
+++ app/models/system_stability.rb
@@ -1,3 +1,8 @@
 class SystemStability < ActiveRecord::Base
   has_many :transformer_information
+  before_save :insert_null_if_empty
+
+  def insert_null_if_empty
+    self.value = nil if self.value == ''
+  end
 end
