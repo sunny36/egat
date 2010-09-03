55ae45585b379ddbeb7df51905093ae2698b520e
Somchok Sakjiraphong <s@sunny.in.th>
2010-09-04 06:48:03 +0700

    Updated TransformerInformations controller.

--- app/controllers/transformer_informations_controller.rb
+++ app/controllers/transformer_informations_controller.rb
@@ -12,13 +12,17 @@ class TransformerInformationsController < ApplicationController
     @transformer_information.build_load_pattern_per_year
     @bus_voltage_hvs = BusVoltageHv.find(:all, :order => 'value')
     @bus_voltage_lvs = BusVoltageLv.find(:all, :order => 'value')
-    @probability_of_force_outages = ProbabilityOfForceOutage.find(:all, :order => 'value')    
+    @probability_of_force_outages = ProbabilityOfForceOutage.find(:all, 
+      :conditions => "value IS NOT NULL", :order => 'value')    
     @social_aspects = SocialAspect.find(:all, :order => 'value')
     @system_locations = SystemLocation.find(:all, :order => 'value')
     @public_images = PublicImage.find(:all, :order => 'value')
-    @n1_criterias = N1Criteria.find(:all, :conditions => "value IS NOT NULL", :order => 'value')
-    @application_uses = ApplicationUse.find(:all, :conditions => "value IS NOT NULL", :order => 'value')
-    @system_stabilities = SystemStability.find(:all, :conditions => "value IS NOT NULL", :order => 'value')
+    @n1_criterias = N1Criteria.find(:all, 
+        :conditions => "value IS NOT NULL", :order => 'value')
+    @application_uses = ApplicationUse.find(:all,
+        :conditions => "value IS NOT NULL", :order => 'value')
+    @system_stabilities = SystemStability.find(:all, 
+        :conditions => "value IS NOT NULL", :order => 'value')
     @pollutions = Pollution.find(:all, :order => 'value')
   end
   
