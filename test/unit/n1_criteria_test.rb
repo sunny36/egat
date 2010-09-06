f8d6d58c1ebf7d5327c6bd2c471e18b28e0b5902
Somchok Sakjiraphong <s@sunny.in.th>
2010-09-06 15:33:42 +0700

    Removed test unit.

--- test/unit/n1_criteria_test.rb
+++
@@ -1,8 +0,0 @@
-require 'test_helper'
-
-class N1CriteriaTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
-  end
-end
--- test/unit/public_image_test.rb
+++
@@ -1,8 +0,0 @@
-require 'test_helper'
-
-class PublicImageTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
-  end
-end
