7a77c3866f3f0f3b4f232d8c391c7905ba388cdc
Somchok Sakjiraphong <s@sunny.in.th>
2010-09-09 19:40:44 +0700

    Refactored TransformerInformation spec to use fixture.

--- spec/models/transformer_information_spec.rb
+++ spec/models/transformer_information_spec.rb
@@ -8,32 +8,32 @@ describe TransformerInformation do
   end
 
   it "should return correct BusVoltageHv value" do
-    @transformer_information = TransformerInformation.first
+    @transformer_information = transformer_informations(:one)
     @transformer_information.bus_voltage_hv.value.should eql("230")
   end
   
   it "should return System Fault Level Hv MVA" do 
-    @transformer_information = TransformerInformation.first
+    @transformer_information = transformer_informations(:one)
     @transformer_information.system_fault_level_hv_mva.should be_close(17663.2824, 0.1)
   end
   
   it "should return System Fault Level Lv MVA" do 
-    @transformer_information = TransformerInformation.first
+    @transformer_information = transformer_informations(:one)
     @transformer_information.system_fault_level_lv_mva.should be_close(701.1136, 0.1)
   end
   
   it "should return System Fault Level Hv Score" do
-    @transformer_information = TransformerInformation.first
+    @transformer_information = transformer_informations(:one)
     @transformer_information.system_fault_level_hv_score.should eql(3)
   end
   
   it "should return System Fault Level Lv Score" do
-    @transformer_information = TransformerInformation.first
+    @transformer_information = transformer_informations(:one)
     @transformer_information.system_fault_level_lv_score.should eql(1)
   end
 
   it "should return System Fault Level Score" do
-    @transformer_information = TransformerInformation.first
+    @transformer_information = transformer_informations(:one)
     @transformer_information.system_fault_level_score.should eql(3)
   end
   
