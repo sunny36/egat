bf752c801e1b88324fea69d5e8bd527fc71e9cea
Somchok Sakjiraphong <s@sunny.in.th>
2010-09-09 23:26:10 +0700

    Spec for SocialAspect.

---
+++ spec/models/social_aspect_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+
+describe SocialAspect do
+  before(:each) do
+    @valid_attributes = {
+      
+    }
+  end
+
+  it "should return score of 1 when value is นอกเมือง" do
+  end
+end
