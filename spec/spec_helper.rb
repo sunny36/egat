0f4db53042bc729f97b1d335134dca6123337d28
Somchok Sakjiraphong <s@sunny.in.th>
2010-09-09 19:41:18 +0700

    Don't seed before every test.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,7 +11,7 @@ require 'spec/rails'
 # Requires supporting files with custom matchers and macros, etc,
 # in ./support/ and its subdirectories.
 Dir[File.expand_path(File.join(File.dirname(__FILE__),'support','**','*.rb'))].each {|f| require f}
-load "#{Rails.root}/db/seeds.rb"
+
 Spec::Runner.configure do |config|
   # If you're not using ActiveRecord you should remove these
   # lines, delete config/database.yml and disable :active_record
