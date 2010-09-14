61d6454fada12d3807fbcc105c5da8a7af97dac2
Somchok Sakjiraphong <s@sunny.in.th>
2010-09-14 23:48:45 +0700

    Factories file with factory for load pattern per year.

---
+++ spec/factories.rb
@@ -0,0 +1,7 @@
+Factory.define :load_pattern_per_year do |lppy|
+  lppy.lteq_0_pt_6 0
+  lppy.gt_0_pt_6_and_lteq_1 0
+  lppy.gt_1_and_lteq_1_pt_2 0
+  lppy.gt_1_pt_2_and_lteq_1_pt_5 0
+  lppy.gt_1_pt_5 0
+end
