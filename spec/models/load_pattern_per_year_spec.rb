b8f23596962e921e2ffed5a2c65a80adac37def6
Somchok Sakjiraphong <s@sunny.in.th>
2010-09-14 23:46:35 +0700

    Use factory girl to test load pattern per year.

--- spec/models/load_pattern_per_year_spec.rb
+++ spec/models/load_pattern_per_year_spec.rb
@@ -13,34 +13,21 @@ describe LoadPatternPerYear do
   fixtures :load_pattern_per_years
   before(:each) do
     @valid_attributes = {
-      :lteq_0_pt_6 => 3, 
-      :gt_0_pt_6_and_lteq_1 => 7, 
-      :gt_1_and_lteq_1_pt_2 => 2, 
-      :gt_1_pt_2_and_lteq_1_pt_5 => 0, 
-      :gt_1_pt_5 => 0      
     }
   end
 
-  it "should create a new instance given valid attributes" do
-    LoadPatternPerYear.create!(@valid_attributes)
-  end
-
   it "should have a score of 2 given valid attributes" do
-    load_pattern_per_year = LoadPatternPerYear.create!(@valid_attributes)
+    load_pattern_per_year = Factory(:load_pattern_per_year, 
+                                    :lteq_0_pt_6 => 12,
+                                    :gt_0_pt_6_and_lteq_1 => 7,
+                                    :gt_1_and_lteq_1_pt_2 => 2)
+    load_pattern_per_year.save
     load_pattern_per_year.load_pattern_factor.score.should eql(2)
   end
 
-  context "PKCKT1A" do 
-    it "shoud have a score of 1" do 
-      pkckt1a = {
-        :lteq_0_pt_6 => 12, 
-        :gt_0_pt_6_and_lteq_1 => 0, 
-        :gt_1_and_lteq_1_pt_2 => 0, 
-        :gt_1_pt_2_and_lteq_1_pt_5 => 0, 
-        :gt_1_pt_5 => 0      
-      }
-      load_pattern_per_year = LoadPatternPerYear.create!(pkckt1a)
-      load_pattern_per_year.load_pattern_factor.score.should eql(1)
-    end
+  it "shoud have a score of 1" do 
+    load_pattern_per_year = Factory(:load_pattern_per_year, :lteq_0_pt_6 => 12)
+    load_pattern_per_year.save
+    load_pattern_per_year.load_pattern_factor.score.should eql(1)
   end
 end
