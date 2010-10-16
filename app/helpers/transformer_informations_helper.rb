b7731a1d2be9b256c49078a00a37c50c8c809b21
Somchok Sakjiraphong <s@sunny.in.th>
2010-10-17 03:19:13 +0700

    Importance color.

--- app/helpers/transformer_informations_helper.rb
+++ app/helpers/transformer_informations_helper.rb
@@ -1,2 +1,20 @@
 module TransformerInformationsHelper
+  def score_color(score)
+    case score
+    when 1
+      return "<span style=\"padding:0px 40px 0px 40px; background:rgb(146,208,80);\"></span>"
+    when 2
+      return "<span style=\"padding:0px 40px 0px 40px; background:rgb(0,102,255);\"></span>"
+    when 3
+      return "<span style=\"padding:0px 40px 0px 40px; background:rgb(255,255,0);\"></span>"
+    when 4
+      return "<span style=\"padding:0px 40px 0px 40px; background:rgb(252,152,0);\"></span>"
+    when 5
+      return "<span style=\"padding:0px 40px 0px 40px; background:rgb(255,0,0);\"></span>"
+    when 6
+      return "<span style=\"padding:0px 40px 0px 40px; background:rgb(255,0,0);\"></span>"
+    else 
+      return "<span>-</span>"
+    end
+  end
 end
