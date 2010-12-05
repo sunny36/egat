8910650f51c14e2cd432058c4e93693608a86849
Somchok Sakjiraphong <s@sunny.in.th>
2010-12-05 19:23:39 +0700

    Use secrect_token instead of cookie_verifier_secret.

--- config/initializers/cookie_verification_secret.rb
+++ config/initializers/cookie_verification_secret.rb
@@ -5,4 +5,4 @@
 # Make sure the secret is at least 30 characters and all random, 
 # no regular words or you'll be exposed to dictionary attacks.
 # ActionController::Base.cookie_verifier_secret = 'd7e551ae90807e361fc24e1d5a0ea057e06e2132799e050e8ef0c4e151f678a77e3b5ee9336c3a7db47a0c370d21069b2698cfe71a6f1ca944e812cff7a1495c';
-Egat::Application.config.cookie_secret =  'd7e551ae90807e361fc24e1d5a0ea057e06e2132799e050e8ef0c4e151f678a77e3b5ee9336c3a7db47a0c370d21069b2698cfe71a6f1ca944e812cff7a1495c'
+Egat::Application.config.secret_token =  'd7e551ae90807e361fc24e1d5a0ea057e06e2132799e050e8ef0c4e151f678a77e3b5ee9336c3a7db47a0c370d21069b2698cfe71a6f1ca944e812cff7a1495c'
