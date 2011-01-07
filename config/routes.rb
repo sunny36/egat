d92c327620e3654ecb7c31193cd534e0a9a98528
Somchok Sakjiraphong <s@sunny.in.th>
2011-01-07 07:49:48 +0700

    Changed home page to transfomrer_informations#index

--- config/routes.rb
+++ config/routes.rb
@@ -73,7 +73,7 @@ Egat::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+  root :to => "transformer_informations#index"
 
   # See how all your routes lay out with "rake routes"
 
