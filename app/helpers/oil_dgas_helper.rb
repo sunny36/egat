097da4067ef00bab0083d790ea90680c93f2e299
Somchok Sakjiraphong <s@sunny.in.th>
2011-01-08 04:42:58 +0700

    Form for oil dga with gas subscripted.

--- app/helpers/oil_dgas_helper.rb
+++ app/helpers/oil_dgas_helper.rb
@@ -1,2 +1,13 @@
 module OilDgasHelper
+  def hif_color
+
+  end
+
+  def transformer_name_select(form)
+    @transformers = Transformer.order("transformer_name").all
+    form.collection_select(
+      :transformer_id,  @transformers, :id, :transformer_name, 
+      {:include_blank => true}) 
+
+  end
 end
