9f733fe26b018a14bd1d9013d2f00480fd8777d7
Somchok Sakjiraphong <s@sunny.in.th>
2011-01-07 08:24:12 +0700

    Home controller.

---
+++ public/javascripts/controllers/home.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+	glow.ready(function(){
+				new glow.widgets.Sortable(
+					'#content .grid_5, #content .grid_6',
+					{
+						draggableOptions : {
+							handle : 'h2'
+						}
+					}
+				);
+			});
+	
+});
+
