3a80a71efc4110218f3f829f7729dc47606774be
Somchok Sakjiraphong <s@sunny.in.th>
2011-01-15 20:59:51 +0700

    Remove relationship between VisualInspection and VisualInspectionCondition.

--- app/models/visual_inspection.rb
+++ app/models/visual_inspection.rb
@@ -10,5 +10,4 @@
 #
 
 class VisualInspection < ActiveRecord::Base
-  has_many :visual_inspection_condition
 end
--- app/models/visual_inspection_condition.rb
+++ app/models/visual_inspection_condition.rb
@@ -1,4 +1,19 @@
+# == Schema Information
+# Schema version: 20110111130021
+#
+# Table name: visual_inspection_conditions
+#
+#  id                   :integer(4)      not null, primary key
+#  name                 :string(255)
+#  start                :integer(4)
+#  end                  :integer(4)
+#  condition            :string(255)
+#  score                :integer(4)
+#  score_message        :string(255)
+#  visual_inspection_id :integer(4)
+#  created_at           :datetime
+#  updated_at           :datetime
+#
+
 class VisualInspectionCondition < ActiveRecord::Base
-  belongs_to :visual_inspection, :class_name => "VisualInspection", 
-             :foreign_key => "visual_inspection_id"
 end
