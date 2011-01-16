219574f27c0a1bec101c87e19dade7c5f6e4f8c3
Somchok Sakjiraphong <s@sunny.in.th>
2011-01-16 19:49:32 +0700

    Updated visual_inspections layout to show notice.

--- app/controllers/visual_inspections_controller.rb
+++ app/controllers/visual_inspections_controller.rb
@@ -2,25 +2,29 @@ class VisualInspectionsController < ApplicationController
   def index
     @transformer = Transformer.find(params[:transformer_id])
   end
-  
+
   def search
     @transformers = Transformer.all
   end
-  
+
   def new
     @transformer = Transformer.find(params[:transformer_id])
     @visual_inspection = VisualInspection.new
     @visual_inspection.build_general_condition
   end
-  
+
   def create
-    debugger
-    @visual_inspection = VisualInspection.new(params[:visual_inspection])      
-    if @visual_inspection.save
-      flash[:notice] = "Successfully created general condition"
-      redirect_to search_visual_inspections_path
-    else
-      render :controller => 'visual_inspection', :action => 'new'
-    end    
+    @visual_inspection = VisualInspection.new(params[:visual_inspection])
+    respond_to do |format|
+      if @visual_inspection.save
+        format.html {
+          redirect_to(search_visual_inspections_path,
+                      :notice => "Data for Visual Inspection has been added")
+        }
+      else
+        format.html { render :action => "new" }
+      end
+    end
   end
+
 end
