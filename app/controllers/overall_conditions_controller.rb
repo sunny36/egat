c87a92167e2522fabeaa43cfd58b2a973b971bd6
Somchok Sakjiraphong <s@sunny.in.th>
2011-01-17 20:59:13 +0700

    OverallConditions views and layouts.

---
+++ app/controllers/overall_conditions_controller.rb
@@ -0,0 +1,19 @@
+class OverallConditionsController < ApplicationController
+  def index
+    @transformer = Transformer.find(params[:transformer_id])
+    @visual_inspection =
+      VisualInspection.where("transformer_id = ?", params[:transformer_id]).
+      first
+    @general_condition =
+      GeneralCondition.where("visual_inspection_id = ?", @visual_inspection.id).
+      first
+    if request.xhr?
+      @no_js = true
+      @no_header = true
+      @no_footer = true
+    end
+    respond_to do |format|
+      format.html
+    end
+  end
+end
