c906002597c4b1737990ecc2ca60281b05cd352e
Somchok Sakjiraphong <s@sunny.in.th>
2011-01-17 17:51:08 +0700

    Updated spec for general condition.

---
+++ spec/factories/general_condition.rb
@@ -0,0 +1,8 @@
+Factory.define :general_condition do |general_condition|
+  general_condition.maxload 0
+  general_condition.sound 0
+  general_condition.vibration 0
+  general_condition.ground_connector 0
+  general_condition.foundation 0
+  general_condition.animal_protect 0  
+end
--- spec/models/general_condition_spec.rb
+++ spec/models/general_condition_spec.rb
@@ -1,5 +1,30 @@
 require 'spec_helper'
 
 describe GeneralCondition do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @general_condition = Factory.build(:general_condition)
+  end
+
+  it "should return the correct denominator" do
+    @general_condition.save!
+    @general_condition.denominator.should be_close(90.0, 0.01)
+  end
+
+  context "AT_KT7A" do
+    it "should return correct %GCF" do
+      @general_condition.maxload = score(1, :maxload)
+      @general_condition.sound = score(1, :sound)
+      @general_condition.vibration = score(1, :vibration)
+      @general_condition.ground_connector = score(1, :ground_connector)
+      @general_condition.foundation = score(1, :foundation)
+      @general_condition.animal_protect = score(1, :animal_protect)
+      @general_condition.percent_general_condition_factor.should be_close(33.33, 0.01)
+    end
+
+    def score(value, field_name)
+      VisualInspectionCondition.
+      where("name = ? AND table_name = 'general_conditions' AND score = 1", field_name.to_s).first.id
+    end
+  end
+
 end
