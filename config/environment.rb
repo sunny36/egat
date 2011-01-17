1d60e21970f2c1b3f598ceaf7b776a8b512b6823
Somchok Sakjiraphong <s@sunny.in.th>
2011-01-17 10:58:55 +0700

    Commented fields_with_errors html generation.

--- config/environment.rb
+++ config/environment.rb
@@ -3,11 +3,11 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Egat::Application.initialize!
-ActionView::Base.field_error_proc = Proc.new do |html_tag, instance| 
-  if html_tag =~ /<label/
-    t = html_tag.split('</')
-    (%(#{t[0]}<small>**</small></#{t[1]})).html_safe
-  else
-    html_tag
-  end
-end
+# ActionView::Base.field_error_proc = Proc.new do |html_tag, instance| 
+#   if html_tag =~ /<label/
+#     t = html_tag.split('</')
+#     (%(#{t[0]}<small>**</small></#{t[1]})).html_safe
+#   else
+#     html_tag
+#   end
+# end
