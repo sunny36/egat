23f5299b355a19e7278f4e82b4a70a7b13a50931
Somchok Sakjiraphong <s@sunny.in.th>
2011-01-22 19:45:32 +0700

    Removed debugger.

--- app/controllers/general_conditions_controller.rb
+++ app/controllers/general_conditions_controller.rb
@@ -1,7 +1,6 @@
 class GeneralConditionsController < ApplicationController
 
   def index
-    debugger
     @general_condition = 
       GeneralCondition.where("visual_inspection_id = ?", params[:visual_inspection_id]).
       order("test_date DESC").first
