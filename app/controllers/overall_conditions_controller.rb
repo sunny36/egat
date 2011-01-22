95b17c61e1d0e7fa059ce459aee37e46569aceab
Somchok Sakjiraphong <s@sunny.in.th>
2011-01-22 19:48:36 +0700

    Add bushing condition to overall condition page.

--- app/controllers/overall_conditions_controller.rb
+++ app/controllers/overall_conditions_controller.rb
@@ -2,11 +2,12 @@ class OverallConditionsController < ApplicationController
   def index
     @transformer = Transformer.find(params[:transformer_id])
     @visual_inspection =
-      VisualInspection.where("transformer_id = ?", params[:transformer_id]).
-      first
-    @general_condition =
-      GeneralCondition.where("visual_inspection_id = ?", @visual_inspection.id).
-      first
+      VisualInspection.where("transformer_id = ?",params[:transformer_id]).
+      order("test_date DESC").first
+    @general_condition = GeneralCondition.where("visual_inspection_id = ?",
+                                                @visual_inspection.id).first
+    @bushing_condition = BushingCondition.where("visual_inspection_id = ?",
+                                                @visual_inspection.id).first
     if request.xhr?
       @no_js = true
       @no_header = true
--- app/helpers/overall_conditions_helper.rb
+++ app/helpers/overall_conditions_helper.rb
@@ -1,26 +1,43 @@
 module OverallConditionsHelper
-
-  def general_condition_hi_factor(general_condition)
-    unless general_condition.nil?
-      general_condition.hi_factor
-    end
+  
+  def hi_factor(object)
+    unless object.nil?
+      object.hi_factor
+    end    
   end
 
   def general_condition_factor_color(general_condition, visual_inspection, transformer)
     unless general_condition.nil?
-      general_condition_factor = GeneralConditionFactor.
-        where("hi_factor = ?", general_condition.hi_factor).first
-      return link_to("<span style=\"padding:0px 40px 0px 40px; background:rgb(#{general_condition_factor.color});\"></span>".html_safe, 
-                     transformer_visual_inspection_general_conditions_path(transformer, visual_inspection))
-                    
+      general_condition_factor = 
+        GeneralConditionFactor.where("hi_factor = ?", 
+                                     general_condition.hi_factor).first
+      color = "<span style=\"padding:0px 40px 0px 40px; background:rgb(" + 
+              "#{general_condition_factor.color});\">" + "</span>"
+      link = 
+        transformer_visual_inspection_general_conditions_path(transformer, 
+        visual_inspection)
+      return link_to(color.html_safe, link)
     end
   end
   
-  def general_condition_recent_date(visual_inspection)
+  def bushing_condition_factor_color(bushing_condition, visual_inspection, transformer)
+    unless bushing_condition.nil?
+      bushing_condition_factor = 
+        BushingConditionFactor.where("hi_factor = ?", 
+                                     bushing_condition.hi_factor).first
+      color = "<span style=\"padding:0px 40px 0px 40px; background:rgb(" + 
+              "#{bushing_condition_factor.color});\">" + "</span>"
+      link = 
+        transformer_visual_inspection_bushing_conditions_path(transformer, 
+        visual_inspection)
+      return link_to(color.html_safe, link)
+    end
+  end
+  
+  def recent_date(visual_inspection)
     unless visual_inspection.nil?
       visual_inspection.test_date.strftime("%d/%m/%Y")
     end
   end
-
 
 end
