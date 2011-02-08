470ce265ffd6632ab7c287a97abd95e407a05854
Somchok Sakjiraphong <s@sunny.in.th>
2011-02-08 20:22:01 +0700

    Form for TransformerControlCabinet.

--- app/controllers/visual_inspections_controller.rb
+++ app/controllers/visual_inspections_controller.rb
@@ -19,6 +19,7 @@ class VisualInspectionsController < ApplicationController
     @visual_inspection.build_main_tank
     @visual_inspection.build_hot_line_oil_filter
     @visual_inspection.build_radiator_cooling_system
+    @visual_inspection.build_transformer_control_cabinet
   end
 
   def create
