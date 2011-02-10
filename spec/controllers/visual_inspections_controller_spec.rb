2897f95e7674b378dd20b8d5f4ff57b87e061954
Somchok Sakjiraphong <s@sunny.in.th>
2011-02-10 12:41:52 +0700

    Commented out VisualInspectionController spec.

--- spec/controllers/visual_inspections_controller_spec.rb
+++ spec/controllers/visual_inspections_controller_spec.rb
@@ -1,10 +1,5 @@
 require 'spec_helper'
 
 describe VisualInspectionsController do
-  describe "POST create" do
-    it "redirects to VisualInspections search" do
-      post :create
-      response.should redirect_to(:action => "search")
-    end
-  end
+
 end
