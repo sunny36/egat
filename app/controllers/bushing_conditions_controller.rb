5e5617ccbbad73c85d00f0ee7f44d35ea552b067
Somchok Sakjiraphong <s@sunny.in.th>
2011-02-13 16:22:58 +0700

    Fix bug in Bushing Condition index view.

--- app/controllers/bushing_conditions_controller.rb
+++ app/controllers/bushing_conditions_controller.rb
@@ -1,11 +1,11 @@
 class BushingConditionsController < ApplicationController
-  
+
   def index
-    @bushing_condition = 
-      BushingCondition.where("visual_inspection_id = ?", params[:visual_inspection_id]).
-      order("test_date DESC").first    
+    @bushing_condition =
+      BushingCondition.where("visual_inspection_id = ?",
+                             params[:visual_inspection_id]).first
   end
-  
+
   def new
     @transformer = Transformer.find(params[:transformer_id])
     @bushing_condition = BushingCondition.new
