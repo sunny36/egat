9674169d66965bab82cbb271dfd4972687bafdde
Somchok Sakjiraphong <s@sunny.in.th>
2011-02-13 17:52:16 +0700

    ConservatorTank index view.

--- app/controllers/conservator_tanks_controller.rb
+++ app/controllers/conservator_tanks_controller.rb
@@ -1,2 +1,8 @@
-class ConservatorTanksController < ApplicationController
+class ConservatorTanksController < ApplicationController  
+  def index
+    @conservator_tank =
+      ConservatorTank.where("visual_inspection_id = ?",
+                            params[:visual_inspection_id]).first
+  end
+
 end
--- app/helpers/conservator_tanks_helper.rb
+++ app/helpers/conservator_tanks_helper.rb
@@ -1,2 +1,29 @@
 module ConservatorTanksHelper
+  def percent_conservator_tank_factor(conservator_tank)
+    number_with_precision(conservator_tank.percent_conservator_tank_factor, 
+                          :precision => 2)
+  end
+
+  def description(field_name, remove_leading_number = true)
+    description = VisualInspectionCondition.
+      where("name = ? AND table_name = 'conservator_tanks'", field_name.to_s).
+      first.description
+    if remove_leading_number
+      description.split(" ", 2).second
+    else 
+      description
+    end
+  end
+
+  def condition(id)
+    return " - " if id.nil?
+    VisualInspectionCondition.find(id).condition
+  end
+
+  def hi_factor_color(hi_factor)   
+    conservator_tank_factor = 
+      ConservatorTankFactor.where("hi_factor = ?", hi_factor).first
+    "<span style=\"padding:0px 40px 0px 40px; background:rgb(#{conservator_tank_factor.color});\"></span>".html_safe
+  end
+
 end
