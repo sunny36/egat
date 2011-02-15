fcf7d51dbffc42015ec28210505bf04ed26d26f7
Somchok Sakjiraphong <s@sunny.in.th>
2011-02-16 03:49:11 +0700

    Updated annotations.

--- app/models/thermo_scan_factor.rb
+++ app/models/thermo_scan_factor.rb
@@ -1,2 +1,19 @@
+# == Schema Information
+# Schema version: 20110215204448
+#
+# Table name: thermo_scan_factors
+#
+#  id          :integer(4)      not null, primary key
+#  hi_factor   :integer(4)
+#  condition   :string(255)
+#  load_start  :integer(4)
+#  load_end    :integer(4)
+#  delta_start :integer(4)
+#  delta_end   :integer(4)
+#  color       :string(255)
+#  created_at  :datetime
+#  updated_at  :datetime
+#
+
 class ThermoScanFactor < ActiveRecord::Base
 end
--- app/models/transformer_control_cabinet_factor.rb
+++ app/models/transformer_control_cabinet_factor.rb
@@ -1,2 +1,17 @@
+# == Schema Information
+# Schema version: 20110215204448
+#
+# Table name: transformer_control_cabinet_factors
+#
+#  id         :integer(4)      not null, primary key
+#  hi_factor  :integer(4)
+#  condition  :string(255)
+#  start      :integer(4)
+#  end        :integer(4)
+#  color      :string(255)
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 class TransformerControlCabinetFactor < ActiveRecord::Base
 end
