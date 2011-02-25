55cdb1f2a37557b44703ebf10b8eecba9ae378f8
Somchok Sakjiraphong <s@sunny.in.th>
2011-02-26 04:16:05 +0700

    OltcOilQuality business logic.

--- app/models/oltc_oil_quality.rb
+++ app/models/oltc_oil_quality.rb
@@ -1,2 +1,78 @@
 class OltcOilQuality < ActiveRecord::Base
+
+  def hi_factor(insulating_oil, oltc_oil_contamination)
+    OltcOilQualityFactor.all.each do |oltc_oil_quality_factor|
+      oltc_oil_quality_factor.start = 0 if oltc_oil_quality_factor.start.nil?
+      oltc_oil_quality_factor.end = 1000000 if oltc_oil_quality_factor.end.nil?
+      condition = percent_oltc_oil_quality_factor(insulating_oil, oltc_oil_contamination).between?(oltc_oil_quality_factor.start, oltc_oil_quality_factor.end)
+      if condition
+        return oltc_oil_quality_factor.hi_factor
+      end
+    end
+  end
+  
+  def percent_oltc_oil_quality_factor(insulating_oil, oltc_oil_contamination)
+    return nil if insulating_oil.blank? || oltc_oil_contamination.blank?
+    (numerator(insulating_oil, oltc_oil_contamination).to_f / denominator.to_f) * 100
+  end
+  
+  def numerator(insulating_oil, oltc_oil_contamination)
+    dielectric_breakdown_score_times_weight = dielectric_breakdown_score(insulating_oil) *
+    OltcOilQuality.where('name = ?', 'xbar').first.weight.to_i
+    water_content_score_times_weight = water_content_score(oltc_oil_contamination) *
+    OltcOilQuality.where('name = ?', 'water_content').first.weight.to_i
+    color_score_times_weight = color_score(oltc_oil_contamination) *
+    OltcOilQuality.where('name = ?', 'color').first.weight.to_i
+    (dielectric_breakdown_score_times_weight + water_content_score_times_weight + color_score_times_weight).to_f
+  end
+
+  def color_score(oltc_oil_contamination)
+    oltc_oil_qualities = OltcOilQuality.where('name = ?', 'color')
+    oltc_oil_qualities.each do |oltc_oil_quality|
+      oltc_oil_quality.start = 0 if oltc_oil_quality.start.nil?
+      oltc_oil_quality.end = 1000000 if oltc_oil_quality.start.nil?
+      if oltc_oil_contamination.color.between?(oltc_oil_quality.start, oltc_oil_quality.end)
+        return oltc_oil_quality.score
+      end
+    end
+  end
+
+
+  def water_content_score(oltc_oil_contamination)
+    oltc_oil_qualities = OltcOilQuality.where('name = ?', 'water_content')
+    oltc_oil_qualities.each do |oltc_oil_quality|
+      oltc_oil_quality.start = 0 if oltc_oil_quality.start.nil?
+      oltc_oil_quality.end = 1000000 if oltc_oil_quality.start.nil?
+      if oltc_oil_contamination.water_content.between?(oltc_oil_quality.start, oltc_oil_quality.end)
+        return oltc_oil_quality.score
+      end
+    end
+  end
+
+  def dielectric_breakdown_score(insulating_oil)
+    xbar = insulating_oil.xi_average_oltc
+    u = insulating_oil.transformer.hv
+    oltc_oil_qualities = OltcOilQuality.where('name = ?', 'xbar')
+    oltc_oil_qualities.each do |oltc_oil_quality|
+      oltc_oil_quality.start = 0 if oltc_oil_quality.start.nil?
+      oltc_oil_quality.end = 1000000 if oltc_oil_quality.end.nil?
+      if (u.between?(oltc_oil_quality.u_start, oltc_oil_quality.u_end) &&
+          xbar.between?(oltc_oil_quality.start, oltc_oil_quality.end))
+        return oltc_oil_quality.score
+      end
+    end
+  end
+
+  def denominator
+    sum = 0
+    ['xbar', 'water_content', 'color'].each do |field|
+      oltc_oil_quality = OltcOilQuality.where('name = ?', field).order('score DESC').first
+      debugger
+      max_score = oltc_oil_quality.score.to_i
+      weight = oltc_oil_quality.weight.to_i
+      sum = sum + (max_score * weight)
+    end
+    return sum
+  end
+
 end
--- spec/models/oltc_oil_quality_spec.rb
+++ spec/models/oltc_oil_quality_spec.rb
@@ -1,5 +1,41 @@
 require 'spec_helper'
 
 describe OltcOilQuality do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @oltc_oil_quality  = OltcOilQuality.new
+    InsulatingOil.any_instance.stubs(:xi_average_oltc => 40, :transfromer_id => 1)
+    InsulatingOil.any_instance.expects(:transformer).returns(Transformer.find(1))
+    @insulating_oil = InsulatingOil.new
+    OltcOilContamination.any_instance.stubs(:water_content => 35, :color => 0.9)
+    @oltc_oil_contamination = OltcOilContamination.new
+  end
+
+  it "should return the correct value for denominator" do
+    @oltc_oil_quality.denominator.should eql(42)
+  end
+
+  it "should return the correct score for xbar" do
+    @oltc_oil_quality.dielectric_breakdown_score(@insulating_oil).should eql(2)
+  end
+
+  it "should return the correct score for water_content" do
+    @oltc_oil_quality.water_content_score(@oltc_oil_contamination).should eql(3)
+  end
+
+  it "should return the correct score for color" do
+    @oltc_oil_quality.color_score(@oltc_oil_contamination).should eql(1)
+  end
+
+  it "should return the correct value for numerator" do
+    @oltc_oil_quality.numerator(@insulating_oil, @oltc_oil_contamination).should be_close(20.0, 0.01)
+  end
+
+  it "should return the correct value for %OOQ Factor" do
+    @oltc_oil_quality.percent_oltc_oil_quality_factor(@insulating_oil,
+                                                      @oltc_oil_contamination).should be_close(47.62, 0.01)
+  end
+  
+  it "should return the correct HIF" do
+    @oltc_oil_quality.hi_factor(@insulating_oil, @oltc_oil_contamination).should eql(1)
+  end
 end
