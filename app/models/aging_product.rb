bfe9c2adf9022c7c51ffd6f90044d982801f7639
Somchok Sakjiraphong <s@sunny.in.th>
2011-03-15 03:22:08 +0700

    Updated AgingProduct calculation.

--- app/models/aging_product.rb
+++ app/models/aging_product.rb
@@ -1,2 +1,33 @@
 class AgingProduct < ActiveRecord::Base
+
+  def hi_factor(oil_contamination)
+
+  end
+
+  def percent_aging_product_factor(oil_contamination)
+    return nil if oil_contamination.blank?
+    return (numerator(oil_contamination).to_f / denominator.to_f) * 100.0
+  end
+
+  def numerator(oil_contamination)
+    u = oil_contamination.transformer.hv
+    ift_by_nn = oil_contamination.ift.to_f/oil_contamination.nn.to_f
+    score = weight = nil
+    AgingProduct.where(:name => "ift/nn").each do |aging_product|
+      aging_product.u_end = 1000000 if aging_product.u_end.nil?
+      if (u.between?(aging_product.u_start, aging_product.u_end) &&
+          ift_by_nn.round.between?(aging_product.start, aging_product.end))
+          score = aging_product.score
+          weight = aging_product.weight
+      end
+    end
+    score * weight
+  end
+
+  def denominator
+    aging_product = AgingProduct.where(:name => "ift/nn").order("score DESC").first
+    max_score = aging_product.score.to_i
+    weight = aging_product.weight.to_i
+    max_score * weight
+  end
 end
--- spec/models/aging_product_spec.rb
+++ spec/models/aging_product_spec.rb
@@ -1,5 +1,24 @@
 require 'spec_helper'
 
 describe AgingProduct do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @aging_product  = AgingProduct.new
+    OilContamination.any_instance.stubs(:ift => 25.0, :nn => 0.1, :water_content => 38, :color => 1.8,
+                                        :transformer_id => 1)
+    OilContamination.any_instance.stubs(:transformer).returns(Transformer.find(1))
+    @oil_contamination = OilContamination.new
+  end
+
+  it "should return the correct value for denominator" do
+    @aging_product.denominator.should eql(20)
+  end
+  
+  it "should return the correct value for numerator" do
+    @aging_product.numerator(@oil_contamination).should be_close(10, 0.01)
+  end
+
+  it "should return the correct %APF " do
+    @aging_product.percent_aging_product_factor(@oil_contamination).should be_close(50.0, 0.01)
+  end
+
 end
