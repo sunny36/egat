ff5aa87d8857d36a5ede6764aec05d8a919f390a
Somchok Sakjiraphong <s@sunny.in.th>
2011-03-20 00:37:39 +0700

    Calculate %HI for Insulating Oil.

--- app/models/insulating_oil_factor.rb
+++ app/models/insulating_oil_factor.rb
@@ -1,2 +1,11 @@
 class InsulatingOilFactor < ActiveRecord::Base
+  def self.percent_hi(insulating_oil, oil_contamination)
+    contamination_hi_factor = OilQuality.new.hi_factor(insulating_oil, oil_contamination, :contamination)
+    aging_product_hi_factor = AgingProduct.new.hi_factor(oil_contamination)
+    oil_contamination_weight = InsulatingOilWeight.find_by_name("oil_contamination").weight
+    aging_product_weight = InsulatingOilWeight.find_by_name("aging_product").weight
+    numerator = (contamination_hi_factor * oil_contamination_weight) + (aging_product_hi_factor * aging_product_weight)
+    denominator = (4 * oil_contamination_weight) + (4 * aging_product_weight)
+    numerator/denominator
+  end
 end
--- spec/models/insulating_oil_factor_spec.rb
+++ spec/models/insulating_oil_factor_spec.rb
@@ -1,5 +1,19 @@
 require 'spec_helper'
 
 describe InsulatingOilFactor do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @oil_quality  = OilQuality.new
+    InsulatingOil.any_instance.stubs(:xi_average_maintank => 26.0, :avg_percent_power_factor_maintank => 2.14,
+                                     :cor_percent_power_factor_maintank => 6,
+                                     :transformer_id => 1)
+    InsulatingOil.any_instance.stubs(:transformer).returns(Transformer.find(1))
+    @insulating_oil = InsulatingOil.new
+    OilContamination.any_instance.stubs(:ift => 25.0, :nn => 0.1, :water_content => 38, :color => 1.8, :transformer_id => 1)
+    OilContamination.any_instance.stubs(:transformer).returns(Transformer.find(1))
+    @oil_contamination = OilContamination.new
+  end
+  
+  it "should return the correct percent HI for Insulating Oil" do
+    InsulatingOilFactor.percent_hi(@insulating_oil, @oil_contamination).should be_close(0, 0.01)
+  end
 end
