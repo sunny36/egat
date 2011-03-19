7cfb2a746cfc05812cbc1afe64a3d5119823c66d
Somchok Sakjiraphong <s@sunny.in.th>
2011-03-19 23:50:05 +0700

    Updated OilQuality fixture.

--- db/fixtures/oil_qualities.rb
+++ db/fixtures/oil_qualities.rb
@@ -123,11 +123,26 @@ OilQuality.seed(
    :color_id => red.id},
   #-----------------------------------------------------------------------------------------------------
 
-  # Water Content
+  # Color
   {:name => 'color', :start => nil, :end => 1.0, :score => 1, :weight => 2, :color_id => green.id},
   {:name => 'color', :start => 1.1, :end => 2.5, :score => 2, :weight => 2, :color_id => blue.id},
   {:name => 'color', :start => 2.6, :end => 4.0, :score => 3, :weight => 2, :color_id => yellow.id},
-  {:name => 'color', :start => 4.1, :end => nil, :score => 4, :weight => 2, :color_id => red.id}
+  {:name => 'color', :start => 4.1, :end => nil, :score => 4, :weight => 2, :color_id => red.id},
   #-----------------------------------------------------------------------------------------------------
+  
+  # Power Factor @ 20C
+  {:name => 'pf_20c', :start => nil, :end => 0.1, :score => 1, :weight => 2, :color_id => green.id},
+  {:name => 'pf_20c', :start => 0.2, :end => 0.5, :score => 2, :weight => 2, :color_id => blue.id},
+  {:name => 'pf_20c', :start => 0.6, :end => 1.0, :score => 3, :weight => 2, :color_id => yellow.id},
+  {:name => 'pf_20c', :start => 1.1, :end => nil, :score => 4, :weight => 2, :color_id => red.id},
+  #-----------------------------------------------------------------------------------------------------
+
+  # Power Factor @ 100C
+  {:name => 'pf_100c', :start => nil, :end => 1.0, :score => 1, :weight => 2, :color_id => green.id},
+  {:name => 'pf_100c', :start => 1.1, :end => 5.0, :score => 2, :weight => 2, :color_id => blue.id},
+  {:name => 'pf_100c', :start => 5.1, :end => 7.0, :score => 3, :weight => 2, :color_id => yellow.id},
+  {:name => 'pf_100c', :start => 7.1, :end => nil, :score => 4, :weight => 2, :color_id => red.id}
+  #-----------------------------------------------------------------------------------------------------
+  
 
 )
